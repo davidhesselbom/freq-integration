5c26e9a036dfbf8131acb0600d8bcd0d6e41e1f7
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2012-08-11 00:04:06 +0200

    Using qtfeatures

--- sonicawe-end2end/common/common.sh
+++ sonicawe-end2end/common/common.sh
@@ -3,7 +3,7 @@ set -e
 
 # test that build output exists
 outputname=libcommon.a
-if [ "$(uname -s)" == "MINGW32_NT-6.1" ]; then
+if [ -n "$(uname -s | grep MINGW32_NT)" ]; then
     outputname=release/common.lib
 fi
 
