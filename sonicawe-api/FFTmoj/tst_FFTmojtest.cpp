e238cb9b2074f3a760768377582e7e813c373639
David Hesselbom <david.hesselbom@gmail.com>
2012-09-18 17:10:38 +0200

    First serious attempts at timing
    Signed-off-by: David Hesselbom <david.hesselbom@gmail.com>

--- sonicawe-api/FFTmoj/tst_FFTmojtest.cpp
+++ sonicawe-api/FFTmoj/tst_FFTmojtest.cpp
@@ -39,13 +39,22 @@ private Q_SLOTS:
 
 private:
 	complex<float> max(ChunkData::Ptr P);
+	complex<float> max(complex<float> p, complex<float> r);
+	complex<float> max(ChunkData::Ptr P, ChunkData::Ptr R);
 	complex<float> min(ChunkData::Ptr P);
+	complex<float> min(complex<float> p, complex<float> r);
+	complex<float> min(ChunkData::Ptr P, ChunkData::Ptr R);
 	complex<float> mean(ChunkData::Ptr P);
 	ChunkData::Ptr abs(ChunkData::Ptr P);
 	ChunkData::Ptr diff(ChunkData::Ptr P, ChunkData::Ptr R);
 	ChunkData::Ptr mult(ChunkData::Ptr P, ChunkData::Ptr R);
 	float maxerr(ChunkData::Ptr P, ChunkData::Ptr R);
 	float nrmsd(ChunkData::Ptr P, ChunkData::Ptr R);
+
+#define maxerrlim 0.00001 //1e-5
+#define nrmsdlim 0.00000001 //1e-8
+#define startSize 1 << 8
+#define endSize 1 << 22
 };
 
 FFTmojTest::FFTmojTest()
@@ -71,6 +80,19 @@ complex<float> FFTmojTest::max(ChunkData::Ptr P)
 	return max;
 }
 
+complex<float> FFTmojTest::max(complex<float> p, complex<float> r)
+{
+	complex<float> max = p;
+	if (std::abs(r) > std::abs(p))
+		max = r;
+	return max;
+}
+
+complex<float> FFTmojTest::max(ChunkData::Ptr P, ChunkData::Ptr R)
+{
+	return max(max(P), max(R));
+}
+
 complex<float> FFTmojTest::min(ChunkData::Ptr P)
 {
 	complex<float> *p = P->getCpuMemory();
@@ -81,6 +103,19 @@ complex<float> FFTmojTest::min(ChunkData::Ptr P)
 			min = p[i];
 	}
 	return min;
+}
+
+complex<float> FFTmojTest::min(complex<float> p, complex<float> r)
+{
+	complex<float> min = p;
+	if (std::abs(r) < std::abs(p))
+		min = r;
+	return min;
+}
+
+complex<float> FFTmojTest::min(ChunkData::Ptr P, ChunkData::Ptr R)
+{
+	return min(min(P), min(R));
 }
 
 complex<float> FFTmojTest::mean(ChunkData::Ptr P)
@@ -137,14 +172,14 @@ ChunkData::Ptr FFTmojTest::abs(ChunkData::Ptr P)
 float FFTmojTest::maxerr(ChunkData::Ptr P, ChunkData::Ptr R)
 {
 	complex<float> error = max(abs(diff(P, R)));
-	complex<float> difference = max(P)-min(P);
+    complex<float> difference = (max(P, R)-min(P, R));
 	error /= std::abs(difference);
 	return error.real();
 }
 
 float FFTmojTest::nrmsd(ChunkData::Ptr P, ChunkData::Ptr R)
 {
-    complex<float> normRMSD = sqrt(mean(mult(abs(diff(P, R)), abs(diff(P, R))))) / (max(P)-min(P));
+    complex<float> normRMSD = sqrt(mean(mult(abs(diff(P, R)), abs(diff(P, R))))) / (max(P, R)-min(P, R));
 	return normRMSD.real();
 }
 
@@ -164,14 +199,14 @@ void FFTmojTest::testCase1()
 			FftClAmdFft fft = FftClAmdFft();
 		    string techlib = "ClAmdFft";
 		#else
-			static FftClFft fft;
+            FftClFft fft;
 		    string techlib = "ClFft";
 		#endif
 	#elif USE_CUDA
-		static FftCufft;
+        FftCufft fft;
 	    string techlib = "CuFft";
 	#else 
-		static FftOoura fft;
+        FftOoura fft;
 	    string techlib = "Ooura";
 	#endif
 
@@ -180,9 +215,6 @@ void FFTmojTest::testCase1()
 	ofstream outputfile(filename.str().c_str());
 	scriptname << techlib << "Sizes" << ".m";
 	ofstream outputscript(scriptname.str().c_str());
-
-	const int startSize = 1 << 8;
-	const int endSize = 1 << 22;
 
 	int i = startSize;
 	while (i <= endSize)
@@ -207,20 +239,19 @@ void FFTmojTest::testCase1()
 
 void FFTmojTest::testCase2()
 {
-
 	#ifdef USE_OPENCL
 		#ifdef USE_AMD
 			FftClAmdFft fft = FftClAmdFft();
 		    string techlib = "ClAmdFft";
 		#else
-			static FftClFft fft;
+            FftClFft fft;
 		    string techlib = "ClFft";
 		#endif
 	#elif USE_CUDA
-		static FftCufft;
+        FftCufft fft;
 	    string techlib = "CuFft";
 	#else 
-		static FftOoura fft;
+        FftOoura fft;
 	    string techlib = "Ooura";
 	#endif
 
@@ -232,23 +263,33 @@ void FFTmojTest::testCase2()
 	scriptname << techlib << "Comparison" << ".m";
 	ofstream outputscript(scriptname.str().c_str());
 
+	ostringstream correctnessName;
+	correctnessName << techlib << "Correctness" << ".dat";
+	ofstream correctnessFile(correctnessName.str().c_str());
+
 	srand((unsigned)time(0));
-
-	const int startSize = 1 << 10;
-	const int endSize = 1 << 22;
-
-	const float maxerrlim = 0.00001; //1e-5
-	const float nrmsdlim = 0.00000001; //1e-8
 
 	int size = 0;
 	int i = 0;
 	while (i <= endSize)
 	{
 		sizes >> size;
-		if (i == size) //better to check if EOF, but yeah
+		if (size == i) //better to check if EOF, but yeah
 			break;
+
 		else
 			i = size;
+
+	#if defined(USE_OPENCL)
+	#if !defined(USE_AMD)
+		if (size == 1 << 10 || size == 1 << 22)
+		{
+			cout << "Size " << size << " not good for ClFft. Skipping..." << endl << endl;
+			continue;
+		}
+	#endif
+	#endif
+
 		ostringstream realfile, imagfile;
 		realfile << "rand" << i << ".dat";
 		imagfile << "rand" << i << "i" << ".dat";
@@ -276,23 +317,12 @@ void FFTmojTest::testCase2()
 			std::getline(imagdata, tempstring);
 		}
 
-		vector<float> rvalues;
-		vector<float> ivalues;
-		float tempfloatv = 1;
-		float tempfloatw = 0;
-		float tempfloatr = (float)rand()/(float)RAND_MAX;
-
-
 		for (int j = 0; j < i; j++)
 		{
-			tempfloatr = (float)rand()/(float)RAND_MAX;
 			realdata >> tempfloat;
-			p[j].real(tempfloatw);
-			rvalues.push_back(p[j].real());
-			tempfloatr = (float)rand()/(float)RAND_MAX;
+			p[j].real(tempfloat);
 			imagdata >> tempfloat;
-			p[j].imag(tempfloatv);
-			ivalues.push_back(p[j].imag());
+			p[j].imag(tempfloat);
 		}
 
 		realdata.close();
@@ -300,8 +330,8 @@ void FFTmojTest::testCase2()
 
 		cout << "  done!" << endl;
 
-		cout << "(p[0]: " << p[0].real() << ", " << p[0].imag() << ")" << endl;
-		cout << "(p[" << i-1 << "]: " << p[i-1].real() << ", " << p[i-1].imag() << ")" << endl;
+		cout << "(p[0]: " << p[0].real() << ", " << p[0].imag();
+		cout << ", p[" << i-1 << "]: " << p[i-1].real() << ", " << p[i-1].imag() << ")" << endl;
 
 		cout << "Computing FFT...";
 
@@ -358,30 +388,25 @@ void FFTmojTest::testCase2()
 		float nRSMD = nrmsd(facit, result);
 		cout << " done!" << endl;
 
+		correctnessFile << i << " " << maxerror << " " << nRSMD << endl;
+
 		cout << "MAXERR is " << maxerror;
 		if (maxerror > maxerrlim)
-		{
 			cout << " > " << maxerrlim << ": FAIL!" << endl;
-		}
 		else
-		{
 			cout << " < " << maxerrlim << ": WIN!" << endl;
-		}
 
 		cout << "NRMSD is " << nRSMD;
 		if (nRSMD > nrmsdlim)
-		{
 			cout << " > " << nrmsdlim << ": FAIL!" << endl;
-		}
 		else
-		{
 			cout << " < " << nrmsdlim << ": WIN!" << endl;
-		}
 
 		//QVERIFY(maxerror < maxerrlim);
 		//QVERIFY(nRSMD < nrmsdlim);
 
-		/*			
+	#if defined(DUMP_RESULTS)
+
 		cout << "    done!" << endl;
 
 		cout << "Dumping results to file...";
@@ -428,200 +453,87 @@ void FFTmojTest::testCase2()
 		outputscript << "facit = a + b * i;" << "\n";
 		outputscript << "length(result)" << "\n";
 		outputscript << "comparison" << "\n";
-		*/
+	#endif
 		cout << endl;
 	}
 
 	sizes.close();
 	outputscript.close();
-
+	correctnessFile.close();
 }
 
 void FFTmojTest::testCase3()
 {
-    using namespace std;
-    using namespace Tfr;
-    string techlib = "";
-	return;
-#ifdef USE_OPENCL
-    #ifdef USE_AMD
-        FftClAmdFft fft = FftClAmdFft();
-        ofstream outputfile("ClAmdFft");
-    #else
-        static FftClFft fft;
-        ofstream outputfile("ClFft");
-    #endif
-#elif USE_CUDA
-    static FftCufft;
-    ofstream outputfile("CuFft");
-#else 
-	static FftOoura fft;
-    ofstream outputfile("Ooura");
-#endif
-    ChunkData::Ptr data;
-//    ifstream inputfile("rand12.dat");
-    const int size1 = 1 << 18;
-	const int size2 = 1 << 18;
+	#ifdef USE_OPENCL
+		#ifdef USE_AMD
+			FftClAmdFft fft = FftClAmdFft();
+		    string techlib = "ClAmdFft";
+		#else
+            FftClFft fft;
+		    string techlib = "ClFft";
+		#endif
+	#elif USE_CUDA
+        FftCufft fft;
+	    string techlib = "CuFft";
+	#else 
+        FftOoura fft;
+	    string techlib = "Ooura";
+	#endif
 
-    //float input[size1];
-//    for (int i = 0; i < size1; i++)
-//	{
-        //inputfile >> input[i];
-//        input[i] = ((float)rand()/(float)RAND_MAX);
-//    }
-//    inputfile.close();
+	ostringstream sizefile;
+	sizefile << techlib << "Sizes" << ".dat";
+	ifstream sizes(sizefile.str().c_str());
 
-//    srand ( time(NULL) );
+	ostringstream scriptname;
+	scriptname << techlib << "Comparison" << ".m";
+	ofstream outputscript(scriptname.str().c_str());
+	srand((unsigned)time(0));
 
-    // Chunk size:
-    // fails on fusion cpu at 200000000 (134217728)
-    // largest ok on fusion cpu at 100000000 (67108864), 30 sec
-    // largest ok on rampage cpu at 100000000 (67108864), 12.7 sec
-    // fails on fusion gpu at 40000000
-    // largest ok on fusion gpu at 20000000, 15 sec
+	int size = 0;
+	int i = 0;
+	while (i <= endSize)
+	{
+		sizes >> size;
+		if (size == i) //better to check if EOF, but yeah
+			break;
 
-    // We want to test for (2^10, 2^20)
+		else
+			i = size;
 
-        // Or maybe 2^8 to 2^22?
+	#if defined(USE_OPENCL)
+	#if !defined(USE_AMD)
+		if (size == 1 << 10 || size == 1 << 22)
+		{
+			cout << "Size " << size << " not good for ClFft. Skipping..." << endl << endl;
+			continue;
+		}
+	#endif
+	#endif
+		float tempfloatr;
 
-    int lastsize = 0;
-    int uniques = 0;
-
-
-
-
-    for (int N = size1; N <= size2; N++)
-    {
-		const int currentsize = fft.lChunkSizeS(N);
-		
-		if (lastsize == currentsize) { continue; }
-        else { lastsize = currentsize; }
-        uniques++;
-		cout << "size: " << currentsize << "\n";
-
-        data.reset(new ChunkData(currentsize));
+		ChunkData::Ptr data;
+        data.reset(new ChunkData(i));
 		complex<float> *p = data->getCpuMemory();
 
+        ChunkData::Ptr result(new ChunkData(i));
 
-        {
-//            for (int i = 0; i < N; i++)
-//            {
-//                p[i].real(0);
-//                p[i].imag(1);
-//            }
+		for (int j = 0; j < i; j++)
+		{
+			tempfloatr = (float)rand()/(float)RAND_MAX;
+			p[j].real(tempfloatr);
+			tempfloatr = (float)rand()/(float)RAND_MAX;
+			p[j].imag(tempfloatr);
+		}
 
-            ChunkData::Ptr result(new ChunkData(N));
-//            TaskTimer timer("Running FFT, run #1");
-              fft.compute(data, result, FftDirection_Forward);
-//            fft.compute(data, data, DataStorageSize(1, 4), FftDirection_Forward);
-//            timeElapsed = timer.elapsedTime();
-        }
+        cout << "Doing fft of " << i << " " << (1 << 24)/i << " times..." << endl;
 
+		for (int j = 0; j <= (1 << 24)/i; j++)
+		{
+			fft.compute(data, result, FftDirection_Forward);
+		}
 
-		// Compute on new data with the same size, now that we have a plan
-//        data.reset(new ChunkData(currentsize));
-//        p = data->getCpuMemory();
-
-        {
-/*
-            for (int i = 0; i < N; i++)
-            {
-                //p[i].real((float)rand()/(float)RAND_MAX);
-                //p[i].imag((float)rand()/(float)RAND_MAX);
-                //p[i].real(input[i]);
-                //p[i].imag(0);
-            }
-*/
-//			cout << p[0].real() << ", " << p[0].imag() << endl;
-
-//            ChunkData::Ptr result(new ChunkData(N));
-//			TaskTimer timer("Running ClFft, run #2");
-//            fft.compute(data, data, FftDirection_Forward);
-//			complex<float> *r = result->getCpuMemory();
-//			ofstream outputfile("rand12clfft.dat");
-            //p = data->getCpuMemory();
-/*
-            for(int j = 0; j < N; j++)
-			{
-				outputfile << p[j] << endl;
-			}
-			outputfile.close();
-*/
-        }
+        cout << "Done!" << endl << endl;
     }
-
-    cout << "uniques: " << uniques << endl;
-	lastsize = 0;
-	uniques = 0;
-
-
-    for (int N = size1; N <= size2; N++)
-    {
-		float timeElapsed = 0;
-		const int currentsize = fft.lChunkSizeS(N);
-
-        if (lastsize == currentsize) { continue; }
-        else { lastsize = currentsize; }
-        uniques++;
-        cout << "size: " << currentsize << endl;
-
-        data.reset(new ChunkData(currentsize));
-        complex<float> *p = data->getCpuMemory();
-
-        /*
-        {
-            for (int i = 0; i < N; i++)
-            {
-                p[i].real(0);
-                p[i].imag(1);
-            }
-
-            //ChunkData::Ptr result(new ChunkData(N));
-            TaskTimer timer("Running ClFft, run #1");
-            fft.compute(data, data, DataStorageSize(1, 4), FftDirection_Forward);
-        }
-        */
-
-        // Compute on new data with the same size, now that we have a plan
-//        data.reset(new ChunkData(currentsize));
-//        p = data->getCpuMemory();
-
-        {
-/*
-            for (int i = 0; i < N; i++)
-            {
-                //p[i].real((float)rand()/(float)RAND_MAX);
-                //p[i].imag((float)rand()/(float)RAND_MAX);
-                //p[i].real(input[i]);
-                //p[i].imag(0);
-            }
-*/
-//			cout << p[0].real() << ", " << p[0].imag() << endl;
-
-            ChunkData::Ptr result(new ChunkData(currentsize));
-            TaskTimer timer("Running Fft, run #3");
-			int reps = (1 << 24) / N;
-            for (int i = 0; i < reps; i++)
-            {
-                fft.compute(data, data, FftDirection_Forward);
-                complex<float> *r = data->getCpuMemory();
-            }
-            complex<float> *r = data->getCpuMemory();
-            timeElapsed = timer.elapsedTime();
-            outputfile << currentsize << " " << reps << " " << timeElapsed << endl;
-//			ofstream outputfile("rand12clfft.dat");
-            //p = data->getCpuMemory();
-/*
-            for(int j = 0; j < N; j++)
-            {
-                outputfile << p[j] << endl;
-            }
-            outputfile.close();
-*/
-        }
-    }
-
-    outputfile.close();
 
 //    QVERIFY( cudaSuccess == cudaGLSetGLDevice( 0 ) );
 //    QVERIFY( 0==glewInit() );
