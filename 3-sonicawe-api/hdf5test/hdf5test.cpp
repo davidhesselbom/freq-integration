7d2fd5383fcccf23296dbc7c9101ead101679be4
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2013-01-05 00:02:29 +0100

    hdf5test writes chunks and tests buffers with two channels

--- 3-sonicawe-api/hdf5test/hdf5test.cpp
+++ 3-sonicawe-api/hdf5test/hdf5test.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 #include "adapters/hdf5.h"
 #include "signal/buffer.h"
+#include "tfr/stft.h"
 
 using namespace std;
 using namespace Signal;
 using namespace Adapters;
+using namespace Tfr;
+
+template<typename T>
+bool operator!=(DataStorage<T>& A, DataStorage<T>& B)
+{
+    if (A.size () != B.size ())
+        return true;
+
+    T* a = A.getCpuMemory ();
+    T* b = B.getCpuMemory ();
+    return 0 != memcmp(a, b, A.numberOfBytes ());
+}
+
 
 int main(int, char *[])
 {
     try
     {
         pBuffer data_;
-        data_.reset ( new Buffer(Interval(0, 10), 1, 1));
-        float* p = data_->getChannel (0)->waveform_data ()->getCpuMemory ();
-        for (int i=0; i<data_->number_of_samples (); ++i)
-            p[i] = i*10;
+        data_.reset ( new Buffer(Interval(0, 10), 1, 2));
+        for (unsigned c=0; c<data_->number_of_channels (); ++c)
+        {
+            float* p = data_->getChannel (c)->waveform_data ()->getCpuMemory ();
+            for (int i=0; i<data_->number_of_samples (); ++i)
+                p[i] = i*10 + c*1000;
+        }
 
 
         const char* filename = "hdf5test.h5";
@@ -31,8 +48,27 @@ int main(int, char *[])
 
 
         pBuffer loadedoctaveoutput = Hdf5Buffer::loadBuffer ( "octaveoutput.h5", &dummy1, &dummy2);
-        if ( *loadedoctaveoutput != *data_ )
+        if ( *loadedoctaveoutput != *data_)
             throw runtime_error("Failed octave read");
+
+
+        pMonoBuffer data2( new MonoBuffer(Interval(0,8), 1));
+        *data2 |= *data_->getChannel (0);
+        pChunk chunk = Fft(true)(data2);
+
+        const char* chunkname = "hdf5testchunk.h5";
+        Hdf5Chunk::saveChunk (chunkname, *chunk);
+
+
+        pChunk loadedchunk = Hdf5Chunk::loadChunk ( chunkname );
+        if (*loadedchunk->transform_data != *chunk->transform_data)
+            throw runtime_error("Failed chunk read");
+
+
+        pChunk loadedoctavechunk = Hdf5Chunk::loadChunk ( "octavechunk.h5" );
+        // note that this is an exact comparsion of float data.
+        if (*loadedoctavechunk->transform_data != *chunk->transform_data)
+            throw runtime_error("Failed octave chunk read");
 
 
         cout << "OK" << endl;
--- 3-sonicawe-api/hdf5test/hdf5test.m
+++ 3-sonicawe-api/hdf5test/hdf5test.m
@@ -4,7 +4,7 @@ end
 
 inputfile = 'hdf5test.h5';
 buffer = load(inputfile);
-expectedbuffer = struct('fs',1, 'offset',0, 'overlap',0, 'samples',(0:10:90)');
+expectedbuffer = struct('fs',1, 'offset',0, 'overlap',0, 'samples',[(0:10:90)' (1000:10:1090)']);
 
 if ~isequal(buffer, expectedbuffer)
     error(['Error. ' inputfile ' does not match expected data']);
@@ -16,3 +16,28 @@ for t_n=1:numel(t_N)
 end
 
 save('-hdf5', 'octaveoutput.h5', t_N{:});
+
+inputfile2 = 'hdf5testchunk.h5';
+chunkstruct = load(inputfile2);
+
+if ~isfield(chunkstruct,'chunk')
+    error(['Error. ' inputfile2 ' does not match expected data']);
+end
+
+samples = expectedbuffer.samples(1:8,1);
+d=fft(samples) - chunkstruct.chunk;
+if max(max(abs(d))) > 1e-7
+    error(['Error. ' inputfile2 ' does not match expected data']);
+end
+
+if ~isequal(samples, round(ifft(chunkstruct.chunk)))
+    error(['Error. ' inputfile2 ' does not match expected data']);
+end
+
+% write back the same structure that was read
+t_N=fieldnames(chunkstruct);
+for t_n=1:numel(t_N)
+    eval([t_N{t_n} '= chunkstruct.(t_N{t_n});']);
+end
+
+save('-hdf5', 'octavechunk.h5', t_N{:});
