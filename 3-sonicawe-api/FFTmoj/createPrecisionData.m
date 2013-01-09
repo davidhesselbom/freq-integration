92b0c7130ac3a28e2e02bf5566a6d414eda6b9a7
David Hesselbom <david.hesselbom@gmail.com>
2013-01-09 22:07:31 +0100

    Added convenient lines for single lib runs
    Signed-off-by: David Hesselbom <david.hesselbom@gmail.com>

--- 3-sonicawe-api/FFTmoj/createPrecisionData.m
+++ 3-sonicawe-api/FFTmoj/createPrecisionData.m
@@ -1,7 +1,10 @@
 function createPrecisionData
 	cd scripts;
-	libs = {"Ooura", "ClFft", "ClAmdFft", "CuFft"};
-	%libs = {"Ooura"};
+	%libs = {"Ooura", "ClFft", "ClAmdFft", "CuFft"};
+	%libs = {"ClAmdFft"};
+	libs = {"Ooura"};
+	%libs = {"Clfft"};
+	%libs = {"CuFft"};
 	for lib = libs
 		precisionData(lib{1})
 	end
