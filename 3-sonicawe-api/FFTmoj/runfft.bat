5ae1a0591976dc5e1db14629de0267d89ee189e1
David Hesselbom <david.hesselbom@gmail.com>
2018-06-08 13:01:33 +0200

    The makefile works!
    But it opens a new window on each shell call. Can this be fixed, somehow?

--- 3-sonicawe-api/FFTmoj/runfft.bat
+++ 3-sonicawe-api/FFTmoj/runfft.bat
@@ -1,37 +1,7 @@
 @ECHO OFF
 
-mkdir data
-mkdir data\Ooura
-mkdir data\Ooura\run1
-mkdir data\Ooura\run2
-mkdir data\Ooura\run3
-mkdir data\Ooura\run4
-mkdir data\Ooura\run5
-mkdir data\ClFft
-mkdir data\ClFft\run1
-mkdir data\ClFft\run2
-mkdir data\ClFft\run3
-mkdir data\ClFft\run4
-mkdir data\ClFft\run5
-mkdir data\ClAmdFft
-mkdir data\ClAmdFft\run1
-mkdir data\ClAmdFft\run2
-mkdir data\ClAmdFft\run3
-mkdir data\ClAmdFft\run4
-mkdir data\ClAmdFft\run5
-
 REM set path
 PATH=C:\Qt\4.8.4\bin;C:\sonicawe\lib\sonicawe-winlib\sonicawe_snapshot_win32_base;C:\sonicawe\src\release;%PATH%
 
-REM call FFTmoj 5 times
-echo 1 > argv.txt
-call release\cpu\FFTmoj.exe
-echo 2 > argv.txt
-call release\cpu\FFTmoj.exe
-echo 3 > argv.txt
-call release\cpu\FFTmoj.exe
-echo 4 > argv.txt
-call release\cpu\FFTmoj.exe
-echo 5 > argv.txt
 call release\cpu\FFTmoj.exe
 pause
